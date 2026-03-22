340ef2752c194ba3a3d5b530150d811c416db18b
Marcelo Costa da Mata <mcosta@gmail.com>
2026-03-22 10:50:58 -0300

    Configuração do arquivo main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Criação do repositório
+Criação do arquivo sethings.py
+Configuração do arquivo main.py (Configuração de acesso ao site)
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from teste-uv!")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from sethings import url, user, password
+from time import sleep
 
 
-if __name__ == "__main__":
-    main()
+# Configurações do Selenium
+driver = webdriver.Chrome()
+driver.get(url)
+driver.maximize_window()
+sleep(3)
+
+# Tela de login
+user_input = driver.find_element(By.ID, ":r2:")
+user_input.send_keys(user)
+sleep(1)
+password_input = driver.find_element(By.ID, ":r3:")
+password_input.send_keys(password)
+sleep(1)
+password_input.send_keys(Keys.ENTER)
+sleep(10)
